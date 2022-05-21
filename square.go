e662caadfa019d192910eca14e441bda090e6521
epamanshumishra <anshu_mishra@epam.com>
2022-05-21 12:05:19 +0530

    square solution v1

--- square.go
+++ square.go
@@ -9,14 +9,20 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (s *Square) End() Point {
 	// implement me
+	s.start.x = s.start.x + int(s.a)
+	s.start.y = s.start.y + int(s.a)
+
+	return Point{x: s.start.x, y: s.start.y}
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
-	// implement me
+func (s Square) Perimeter() uint {
+	// implement
+	return 4 * s.a
 }
